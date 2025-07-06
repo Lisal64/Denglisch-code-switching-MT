ce12409e98a92bedf17ff408d1b0665b7a992ce8
Lisal64 <lisa.latzelsperger@strg.at>
2025-07-06 11:23:22 +0200

    conduct additional experiments.

--- finetuning/train.py
+++ finetuning/train.py
@@ -1,13 +1,15 @@
 from finetuning.finetune_mbart_all_experiments import MBARTFineTuner
 
-model_name = "facebook/mbart-large-50-many-to-one-mmt"
-dataset_path = "filtered_datasets/mBART_dataset/ratio"
 
-for context_type in ["baseline", "lang", "pos", "lang+pos"]:
-    exp = MBARTFineTuner(
-        model_name=model_name,
-        dataset_path=dataset_path,
-        strategy_name="ratio",
-        context_type=context_type
-    )
-    exp.run()
+if __name__ == "__main__":
+    model_name = "facebook/mbart-large-50-many-to-one-mmt"
+    dataset_path = "filtered_datasets/mBART_dataset/threshold"
+
+    for context_type in ["baseline", "lang", "pos", "lang+pos"]:
+        exp = MBARTFineTuner(
+            model_name=model_name,
+            dataset_path=dataset_path,
+            strategy_name="threshold",
+            context_type=context_type
+        )
+        exp.run()
--- model_evaluation/test.py
+++ model_evaluation/test.py
@@ -7,32 +7,32 @@ if __name__ == "__main__":
 
     baseline = MBARTEvaluator(
         model_path="facebook/mbart-large-50",
-        dataset_path="filtered_datasets/mBART_dataset/ratio",
-        output_dir="results/mBART_base/test_metrics"
+        dataset_path="filtered_datasets/mBART_dataset/threshold",
+        output_dir="results/mBART_threshold_base/test_metrics"
     )
 
     finetuned = MBARTEvaluator(
-        model_path="models/mBART_ratio_baseline",
-        dataset_path="filtered_datasets/mBART_dataset/ratio",
-        output_dir="results/mBART_ratio_baseline/test_metrics"
+        model_path="models/mBART_threshold_baseline",
+        dataset_path="filtered_datasets/mBART_dataset/threshold",
+        output_dir="results/mBART_threshold_baseline/test_metrics"
     )
 
     lang = MBARTEvaluator(
-        model_path="models/mBART_ratio_lang",
-        dataset_path="filtered_datasets/mBART_dataset/ratio",
-        output_dir="results/mBART_ratio_lang/test_metrics"
+        model_path="models/mBART_threshold_lang",
+        dataset_path="filtered_datasets/mBART_dataset/threshold",
+        output_dir="results/mBART_threshold_lang/test_metrics"
     )
 
     pos = MBARTEvaluator(
-        model_path="models/mBART_ratio_pos",
-        dataset_path="filtered_datasets/mBART_dataset/ratio",
-        output_dir="results/mBART_ratio_pos/test_metrics"
+        model_path="models/mBART_threshold_pos",
+        dataset_path="filtered_datasets/mBART_dataset/threshold",
+        output_dir="results/mBART_threshold_pos/test_metrics"
     )
 
     lang_pos = MBARTEvaluator(
-        model_path="models/mBART_ratio_lang+pos",
-        dataset_path="filtered_datasets/mBART_dataset/ratio",
-        output_dir="results/mBART_ratio_lang+pos/test_metrics"
+        model_path="models/mBART_hybrid_lang+pos",
+        dataset_path="filtered_datasets/mBART_dataset/hybrid",
+        output_dir="results/mBART_hybrid_lang+pos/test_metrics"
     )
 
     for evaluator in [baseline, finetuned, lang, pos, lang_pos]:
